ccd126877aaff22162fa129c6f60294e056f4ee1
sadmusicdork-commits <sadmusicdork@gmail.com>
2026-08-17 20:29:16 +0400

    Update rules.py

--- rules.py
+++ rules.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 def add_rules_command(bot):
-    # 1. Command for your rules channel
+    # Command for your rules channel
     @bot.command()
     @commands.has_permissions(administrator=True)
     async def setup_rules(ctx):
@@ -11,17 +11,5 @@ def add_rules_command(bot):
             description="follow tos\nuse common sense\nno nsfw/gore\nno racial slurs\nno advertisement",
             color=discord.Color.dark_theme()
         )
-        embed.set_footer(text="꒰১ ໒꒱ • Server Rules")
-        await ctx.send(embed=embed)
-
-    # 2. Command for your general channel
-    @bot.command()
-    @commands.has_permissions(administrator=True)
-    async def setup_perms(ctx):
-        embed = discord.Embed(
-            title="Picture Permissions",
-            description="rep **/admire** in status or **boost** for pic perms",
-            color=discord.Color.dark_theme()
-        )
-        embed.set_footer(text="꒰১ ໒꒱ • Media Access")
+        embed.set_footer(text="/admire • Server Rules")
         await ctx.send(embed=embed)
